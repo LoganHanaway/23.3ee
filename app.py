b89827aef94c6a187d593547bf4bcef79bd22018
logan <logan@DESKTOP-3RD0PET.localdomain>
2025-01-30 14:33:30 -0600

    context issues

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 """Flask app for Cupcakes"""
+from flask import Flask
+from models import db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.app = app
+db.init_app(app)
+db.create_all()
